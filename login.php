90b0cb425bac0fc4a34a4d1671ca1dbb4b3ed054
Pravin Soni <pravins@useready.com>
2023-05-12 17:44:32 +0530

    test

--- login.php
+++ login.php
@@ -3,6 +3,7 @@ require 'includes/constants.php';
 require 'includes/config.php';
 require 'includes/functions.php';
 
+$site_url = config('site_url');
 
 $content = nav_menu();
 $content .= '
@@ -17,7 +18,7 @@ $content .= '
 
     }
     .img-block{
-    background:url("../template/assets/images/bhoramdeo.png");
+    background:url("'.$site_url.'/template/assets/images/bhoramdeo.png");
     background-repeat:no-repeat;
     background-size:contain;
     background-position: center center;
@@ -28,7 +29,7 @@ $content .= '
         <div class="col-5 col-xs-12 bg-white p-5">
             <h3 class="pb-3">Login</h3>
             <div class="form-style">
-                <form  name="f1" action="../authentication.php" onsubmit = "return validation()"  novalidate method = "POST">
+                <form  name="f1" action="'.$site_url.'/authentication.php" onsubmit = "return validation()"  novalidate method = "POST">
                     <div class="form-group pb-3">    
                         <select class="form-select" id="role" name="userrole" required>
                             <option value="">Select Role</option>
@@ -51,14 +52,14 @@ $content .= '
             </div>
         </div>
         <div class="col-7 d-none d-md-block" style="position:relative;">
-            <img src="../template/assets/images/kidImage.JPG" style="width:100%; height:100%; position:absolute; top:0; left:0; z-index:1">
+            <img src="'.$site_url.'/template/assets/images/kidImage.JPG" style="width:100%; height:100%; position:absolute; top:0; left:0; z-index:1">
             
         </div>
     </div>
 </div>
-        <script src="../template/jquery-3.6.3.min.js"></script>
-        <script src="../template/assets/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="../template/login.js"></script>
+        <script src="'.$site_url.'/template/jquery-3.6.3.min.js"></script>
+        <script src="'.$site_url.'/template/assets/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="'.$site_url.'/template/login.js"></script>
     </body>
 </html>';
 echo $content;
